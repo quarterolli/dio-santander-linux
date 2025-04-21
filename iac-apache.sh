c8d4785d6d15dd5039a6b9533d59e70dc8294f86
quarterolli <58645471+quarterolli@users.noreply.github.com>
2025-04-21 15:47:46 -0300

    Update iac-apache.sh

--- iac-apache.sh
+++ iac-apache.sh
@@ -1,18 +1,14 @@
 #! /bin/bash
 
-echo "Atualizando sistema e instalando servidor Apache..."
-
-apt-get update && apt-get upgrade -y
-apt-get install apache2 -y
+echo "Atualizando índice de pacotes e instalando servidor Apache..."
+apt-get update && apt-get install apache2 -y
 
 echo "Baixando os arquivos da aplicação..."
-
 cd /tmp
 wget -O - https://github.com/quarterolli/AppShelf/archive/refs/heads/main.tar.gz | tar -xz
+cd AppShelf-main || { echo "Falha no download!"; exit 1; }
 
 echo "Copiando arquivos para o diretório do servidor Apache..."
-
-cd AppShelf-main || exit 1
 cp -R * /var/www/html
 
 echo "Aplicação configurada com sucesso!"
